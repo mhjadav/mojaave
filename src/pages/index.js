9449e7ade38269561ecefabd245cbc9976beb765
Mahipatsinh Jadav <mj@mojaave.com>
2018-08-22 15:41:21 +0530

    Added skype link to connect with me

--- src/pages/index.js
+++ src/pages/index.js
@@ -78,6 +78,7 @@ export default () => (
                           <a title="Twitter" href="https://twitter.com/mhjadav" className="card-link text-black"><i className="fa fa-twitter" /></a>
                           <a title="Email" href="mailto:mj@mojaave.com" className="card-link text-black"><i className="fa fa-envelope" /></a>
                           <a title="Linkedin" href="https://www.linkedin.com/in/mhjadav/" className="card-link text-black"><i className="fa fa-linkedin" /></a>
+                          <a title="Skype" href="skype:mahipat2?chat" className="card-link text-black"><i className="fa fa-skype" /></a>
                         </div>
 
                         </h4>
@@ -205,19 +206,18 @@ export default () => (
           <h1 className="display-5">Contact Me</h1>
           <h6 className="display-6">Let`s build something unique</h6>
           <span className="animate-border mt-4 ml-auto mr-auto"></span>
-        </div>
-        <div className="row justify-content-center">
-        <div className="col-md-6 col-xs-11">
-          <div className="col-xs-12 about-text">
-            <h5 className="display-6">
-              <span className="d-none">Feel free to </span>
-              <span>Connect with me via - </span>
+          <h5 className="display-6 mt-4">
               <a  style={{marginLeft: "5px"}} title="Github" href="https://github.com/mhjadav/mojaave" className="card-link text-black"><i className="fa fa-github" /></a>
               <a title="Twitter" href="https://twitter.com/mhjadav" className="card-link text-black"><i className="fa fa-twitter" /></a>
               <a title="Email" href="mailto:mj@mojaave.com" className="card-link text-black"><i className="fa fa-envelope" /></a>
               <a title="Linkedin" href="https://www.linkedin.com/in/mhjadav/" className="card-link text-black"><i className="fa fa-linkedin" /></a>
+              <a title="Skype" href="skype:mahipat2?chat" className="card-link text-black"><i className="fa fa-skype" /></a>
             </h5>
-            <h5 className="display-6">Or Leave a message below </h5>
+        </div>
+        <div className="row justify-content-center mt-5">
+        <div className="col-md-6 col-xs-11">
+          <div className="col-xs-12 about-text">
+            <h5 className="display-6">Leave a message below </h5>
           </div>
             <form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
             <input type="hidden" name="bot-field" />
